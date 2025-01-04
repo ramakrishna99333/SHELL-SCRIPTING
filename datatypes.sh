bf97202834ca8a476be717714080182ab4f039e6
ramkrishnagaddam <ram99441@gmail.com>
2025-01-04 11:45:55 +0530

    test file

--- datatypes.sh
+++ datatypes.sh
@@ -5,6 +5,12 @@ Num2=$2
 
 echo "The sum of $Num1 and $Num2 is: $(($Num1 + $Num2))"
 
+if [ -z "$Num1" ] || [ -z "$Num2" ]; then
+    echo "Error: Two numeric arguments are required."
+    exit 1
+fi
+
+
 echo "number of arguments: $#"
 echo "all arguments: $@"
 echo "all arguments: $*"
