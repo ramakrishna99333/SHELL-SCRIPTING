93e0c8f61edfa76eae859dacec4918cd3823a255
ramkrishnagaddam <ram99441@gmail.com>
2025-01-04 12:55:47 +0530

    test file

---
+++ 05-conditions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ID=$(id -u)
+
+if [$ID -ne 0] then
+  if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi
+
+yum install mysql -y
+
+if [$? -eq 0] then
+    echo "mysql installed successfully"
+else
+    echo "mysql installation failed"
+fi
+
+yum install git -y
+
+if [$? -eq 0] then
+    echo "git  installed successfully"
+else
+    echo "git installation failed"
+fi
