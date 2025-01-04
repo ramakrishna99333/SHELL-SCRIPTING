e24a16d808100a62ef822529ad2a635a281d3c78
ramkrishnagaddam <ram99441@gmail.com>
2025-01-04 22:58:01 +0530

    updated code

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-ID=$(id -u)
-TIMESTAMP=$(date)
 
-echo "The current timestamp is: $TIMESTAMP"
-if [ $ID -ne 0 ]; then
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
     echo "ERROR:: Please run this script with root access"
     exit 1 # you can give other than 0
 else
-    echo "You are root user" 
-fi
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
 yum install mysql -y
 
-if [ $? -ne 0 ]; then
-echo "mysql installation failed"
-exit 1;
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing MySQL is failed"
+    exit 1
 else
-    echo "mysql installation success"  
+    echo "Installing MySQL is SUCCESS"
 fi
 
 yum install git -y
 
-if [ $? -eq 0 ]; then
-echo "mysql installation failed"
-exit 1;
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing GIT is failed"
+    exit 1
 else
-    echo "git installation success"
+    echo "Installing GIT is SUCCESS"
 fi
