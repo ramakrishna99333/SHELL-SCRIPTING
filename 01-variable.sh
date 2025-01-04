70853718c9f4ed1adddde9787260816d564ce09c
ramkrishnagaddam <ram99441@gmail.com>
2025-01-04 11:28:04 +0530

    test file

--- 01-variable.sh
+++ 01-variable.sh
@@ -4,4 +4,10 @@ PERSON1=Ram
 PERSON2=Lakshman
 
 echo "$PERSON1: HI $PERSON2"
-echo "$PERSON2: hello $PERSON2"
+echo "$PERSON2: HI $PERSON1"
+:'
+read -sp "Enter password for $PERSON1: " PASSWORD1
+echo
+read -sp "Enter password for $PERSON2: " PASSWORD2
+echo
+'
---
+++ 02-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Define the variables
+
+PERSON1=$1
+PERSON2=$2
+# Print the variables
+
+echo "$PERSON1: HI $PERSON2"
+echo "$PERSON2: HI $PERSON1"
