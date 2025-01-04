0ecc11e809e105e309dfbba19c72f61087d415c9
ramkrishnagaddam <ram99441@gmail.com>
2025-01-04 11:31:13 +0530

    test file

---
+++ 03-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATE=$(date)
+echo "add user name"
+read USER_NAME
+echo "add password"
+read PASSWORD
+
+echo "The user name is: $USER_NAME"
+echo "The password is: $PASSWORD"
+
+echo "The date is: $DATE"
